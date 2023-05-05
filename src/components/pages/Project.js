e73f899e5a0b99489ce5d82886eb5256eecb2e1f
Diego Reis <diegoreso@me.com>
2023-05-05 20:38:32 -0300

    create project details

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,6 +1,8 @@
 import styles from './Project.module.css'
 import { useParams } from 'react-router-dom'
 import { useState,useEffect } from 'react'
+import Load from '../layout/Load'
+import Container from '../layout/Container'
 
 
 
@@ -10,24 +12,62 @@ function Project(){
 
   const [project,setProject] = useState([])
 
+  const [showProjectForm,setShowProjectForm] =useState(false)
+
+
  useEffect(()=>{
-  fetch(`http://localhost:5000/projects/${id}`,{
-    method:'GET',
-    header:{
-      'Content-Type': 'application/json'
-    },
-  }).then(res => res.json())
-  .then(data =>{
-    setProject(data)
-  })
-  .catch(err => console.log(err))
+  setTimeout(()=>{
+    fetch(`http://localhost:5000/projects/${id}`,{
+      method:'GET',
+      header:{
+        'Content-Type': 'application/json'
+      },
+    }).then(res => res.json())
+    .then(data =>{
+      setProject(data)
+    })
+    .catch(err => console.log(err))
+  },2000)
  },[id])
 
+
+  function toggleProjectForm(){
+    setShowProjectForm(!showProjectForm)
+  }
   return(
     <>
-    <p>
-      {project.name}
-    </p>
+    
+      {project.name ? 
+<div className={styles.project_details}>
+  <Container customClass="column">
+    <div className={styles.details_container}>
+      <h1>Projeto: {project.name} </h1>
+      <button className={styles.btn} onClick={toggleProjectForm}>{!showProjectForm ? 'Editar Projeto' : 'Fechar'}</button>
+      {!showProjectForm ? (
+        <div className={styles.project_info}>
+          <p>
+            <span>Categoria: </span> {project.category.name}
+          </p>
+          <p>
+            <span>Total de Orçamento: </span>{project.budget}
+          </p>
+          <p>
+            <span>Total Utilizado: </span> {project.cost}
+          </p>
+        </div>
+      ) 
+      : (
+        <div className={styles.project_info}>
+          <p>detalhes do projeto</p>
+        </div>
+
+      )}
+    </div>
+  </Container>
+</div>
+
+       : <Load/>}
+    
     </>
   )
 }
