db15f4053b4498ee057ced1e0ff0a0457fa546b7
Diego Reis <diegoreso@me.com>
2023-05-05 16:09:50 -0300

    change projects and css

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,7 +1,8 @@
 import Message from "../layout/Message"
 import { useLocation } from "react-router-dom"
-
-
+import styles from './Projects.module.css'
+import Container from "../layout/Container"
+import LinkButton from "../layout/LinkButton"
 
 function Projects(){
 
@@ -16,9 +17,15 @@ function Projects(){
 
 
   return(
-    <div>
+    <div className={styles.project_container}> 
+      <div className={styles.title_container}>
       <h1>Meus Projetos</h1>
+      <LinkButton to="/novoprojeto" text="Criar Projeto"/>
+      </div>
       { message &&  <Message msg={message} type="success"/>}
+      <Container customClass="start">
+        <p>Projetos</p>
+      </Container>
     </div>
   )
 }
