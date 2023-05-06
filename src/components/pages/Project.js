b44a945ad5ecc557a50a9bb3c50ae55908fb9335
Diego Reis <diegoreso@me.com>
2023-05-06 10:25:20 -0300

    change project page

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -14,6 +14,8 @@ function Project(){
 
   const [showProjectForm,setShowProjectForm] =useState(false)
 
+  const [showServiceForm,setShowServiceForm] =useState(false)
+
   const [message,setMessage] = useState()
 
   const [type,setType] = useState() 
@@ -31,11 +33,13 @@ function Project(){
       setProject(data)
     })
     .catch(err => console.log(err))
-  },2000)
+  },1000)
  },[id])
 
 
   function editPost(project){
+
+    setMessage("")
 
     if(project.budget < project.cost){
       setMessage('O orçamento não pode ser menor que o custo do projeto')
@@ -60,47 +64,65 @@ function Project(){
 
   }
 
-
-
-
   function toggleProjectForm(){
     setShowProjectForm(!showProjectForm)
   }
+
+  function toggleServiceForm(){
+    setShowServiceForm(!showServiceForm)
+  }
+
+
+
   return(
     <>
-    
-      {project.name ? 
-<div className={styles.project_details}>
-  <Container customClass="column">
-    {message && <Message type={type} msg={message}/>}
-    <div className={styles.details_container}>
-      <h1>Projeto: {project.name} </h1>
-      <button className={styles.btn} onClick={toggleProjectForm}>{!showProjectForm ? 'Editar Projeto' : 'Fechar'}</button>
-      {!showProjectForm ? (
-        <div className={styles.project_info}>
-          <p>
-            <span>Categoria: </span> {project.category.name}
-          </p>
-          <p>
-            <span>Total de Orçamento: </span>{project.budget}
-          </p>
-          <p>
-            <span>Total Utilizado: </span> {project.cost}
-          </p>
-        </div>
-      ) 
-      : (
-        <div className={styles.project_info}>
-          <ProjectForm 
-          handleSubmit={editPost} 
-          btnText="Concluir edição" 
-          projectData={project}/>
-        </div>
+    {project.name ? 
+      <div className={styles.project_details}>
+        <Container customClass="column">
+          {message && <Message type={type} msg={message}/>}
+          <div className={styles.details_container}>
+            <h1>Projeto: {project.name} </h1>
+            <button className={styles.btn} onClick={toggleProjectForm}>{!showProjectForm ? 'Editar Projeto' : 'Fechar'}</button>
+            {!showProjectForm ? (
+              <div className={styles.project_info}>
+                <p>
+                  <span>Categoria: </span> {project.category.name}
+                </p>
+                <p>
+                  <span>Total de Orçamento: </span>{project.budget}
+                </p>
+                <p>
+                  <span>Total Utilizado: </span> {project.cost}
+                </p>
+              </div>
+            ) 
+            : (
+              <div className={styles.project_info}>
+                <ProjectForm 
+                handleSubmit={editPost} 
+                btnText="Concluir edição" 
+                projectData={project}/>
+              </div>
 
-      )}
-    </div>
-  </Container>
-  </div>
+            )}
+          </div>
+          <div className={styles.service_form_container}>
+            <h2>Adicione um serviço</h2>
+            <button className={styles.btn} onClick={toggleServiceForm}>{!showServiceForm ? 'Adicionar Serviço' : 'Fechar'}</button>
+            <div className={styles.project_info} >
+              {showServiceForm && (
+                <div>
+                  <p>formulario servico</p>
+                </div>
+              )}
+            </div>
+          </div>
+          <h2>Serviços: </h2>
+          <Container customClass="start">
+            <p>Itens de Servicos</p>
+          </Container>
+        </Container>
+      </div>
 
        :<Load/>}
     
